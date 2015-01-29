caf189ad84b31c8536edddf2853418f6fff84737
Aeternia-ua <gherasymuk@gmail.com>
2015-01-29 14:10:51 +0200

    final version

--- Employee.java
+++ Employee.java
@@ -1,64 +1,100 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
-public class Employee extends EmployeeList {
-	static EmployeeList[] emplyeesArray;
-	public static void main(String[] args) throws IOException, ParseException {
+import org.apache.commons.lang.builder.CompareToBuilder;
+public class Employee implements Comparable<Employee> {
+	
+	private long id;
+	private String name;
+	private String position;
+	private double salary;
+	
+	public Employee() {
 		
-JSONParser parser = new JSONParser();	
-ObjectMapper mapper = new ObjectMapper();
-	String json = ((JSONArray) parser.parse(new FileReader(System.getProperty("user.dir") + "\\src\\" + "Employee1.json"))).toJSONString();
-	 
-	List<EmployeeList> employees = Arrays.asList(mapper.readValue(json, EmployeeList[].class));
-		
-	for (EmployeeList Employee : employees) {
-		System.out.println("Employee name: " + Employee.getName() + ". ID: " +  Employee.getId() + ". position -> "
-	+ Employee.getPosition() + ". salary: " + Employee.getSalary());
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	@Override
+	public int compareTo(Employee o) {
+		return this.salary < o.getSalary() ? -1 :
+			this.salary > o.getSalary() ? 1 : 0;
+	}
+	
+	public static Comparator<Employee> CompareByName = new Comparator<Employee>() {
+		@Override
+		public int compare(Employee empl, Employee empl2) {
+		return new CompareToBuilder()
+		.append(empl.getName(), empl2.getName())
+		.append(empl.getId(), empl2.getId())
+		.append(empl.getPosition(), empl2.getPosition())
+		.append(empl.getSalary(), empl2.getSalary())
+		.toComparison();
 		}
-	
-	EmployeeList[] emplyeesArray = (EmployeeList[]) employees.toArray();
-	Arrays.sort(emplyeesArray);
-	
-	Arrays.sort(emplyeesArray, EmployeeList.CompareByName);
-	for (EmployeeList employee : emplyeesArray) {
-	System.out.println("Sorting by name : " + employee.getName() + " -> ID: " + employee.getId() + 
-			" " + employee.getPosition() + " : " + employee.getSalary());
-	}
-	
-	Arrays.sort(emplyeesArray, EmployeeList.CompareBySalary);
-	for (EmployeeList employee : emplyeesArray) {
-	System.out.println("Sorting by salary : " + employee.getName() + " -> ID: " + employee.getId() + 
-			" " + employee.getPosition() + " : " + employee.getSalary());
-	}
-	
-//	Arrays.sort(emplyeesArray, EmployeeList.CompareByPosition);
-//	for (EmployeeList employee : emplyeesArray) {
-//	System.out.println("Sorting by position : " + employee.getName() + " -> ID: " + employee.getId() + 
-//			" " + employee.getPosition() + " : " + employee.getSalary());
-//	}
-//	Arrays.sort(emplyeesArray, EmployeeList.CompareById);
-//	for (EmployeeList employee : emplyeesArray) {
-//	System.out.println("Sorting by ID : " + employee.getName() + " -> ID: " + employee.getId() + 
-//			" " + employee.getPosition() + " : " + employee.getSalary());
-//	}
+		};
+		
+		public static Comparator<Employee> CompareBySalary = new Comparator<Employee>() {
+			@Override
+			public int compare(Employee empl, Employee empl2) {
+			return new CompareToBuilder()
+			.append(empl.getSalary(), empl2.getSalary())
+			.append(empl.getName(), empl2.getName())
+			.append(empl.getId(), empl2.getId())
+			.append(empl.getPosition(), empl2.getPosition())
+			
+			.toComparison();
+			}
+			};
+			public static Comparator<Employee> CompareByPosition = new Comparator<Employee>() {
+				@Override
+				public int compare(Employee empl, Employee empl2) {
+				return new CompareToBuilder()
+				.append(empl.getPosition(), empl2.getPosition())
+				.append(empl.getName(), empl2.getName())
+				.append(empl.getId(), empl2.getId())
+				.append(empl.getSalary(), empl2.getSalary())
+				.toComparison();
+				}
+				};
+				public static Comparator<Employee> CompareById = new Comparator<Employee>() {
+					@Override
+					public int compare(Employee empl, Employee empl2) {
+					return new CompareToBuilder()
+					.append(empl.getId(), empl2.getId())
+					.append(empl.getName(), empl2.getName())
+					.append(empl.getPosition(), empl2.getPosition())
+					.append(empl.getSalary(), empl2.getSalary())
+					.toComparison();
+					}
+					};
+			
 
-
-	}
-	}
-	
- 
-
+}
--- EmployeeList.java
+++ EmployeeList.java
@@ -1,100 +1,64 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
-import org.apache.commons.lang.builder.CompareToBuilder;
-public class EmployeeList implements Comparable<EmployeeList> {
-	
-	private long id;
-	private String name;
-	private String position;
-	private double salary;
-	
-	public EmployeeList() {
-		
-	}
-	
-	public long getId() {
-		return id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getPosition() {
-		return position;
-	}
-	
-	public double getSalary() {
-		return salary;
-	}
-	
-	public void setId(long id) {
-		this.id = id;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public void setPosition(String position) {
-		this.position = position;
-	}
-	
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-	
-	@Override
-	public int compareTo(EmployeeList o) {
-		return this.salary < o.getSalary() ? -1 :
-			this.salary > o.getSalary() ? 1 : 0;
-	}
-	
-	public static Comparator<EmployeeList> CompareByName = new Comparator<EmployeeList>() {
-		@Override
-		public int compare(EmployeeList empl, EmployeeList empl2) {
-		return new CompareToBuilder()
-		.append(empl.getName(), empl2.getName())
-		.append(empl.getId(), empl2.getId())
-		.append(empl.getPosition(), empl2.getPosition())
-		.append(empl.getSalary(), empl2.getSalary())
-		.toComparison();
-		}
-		};
-		
-		public static Comparator<EmployeeList> CompareBySalary = new Comparator<EmployeeList>() {
-			@Override
-			public int compare(EmployeeList empl, EmployeeList empl2) {
-			return new CompareToBuilder()
-			.append(empl.getSalary(), empl2.getSalary())
-			.append(empl.getName(), empl2.getName())
-			.append(empl.getId(), empl2.getId())
-			.append(empl.getPosition(), empl2.getPosition())
-			
-			.toComparison();
-			}
-			};
-			public static Comparator<EmployeeList> CompareByPosition = new Comparator<EmployeeList>() {
-				@Override
-				public int compare(EmployeeList empl, EmployeeList empl2) {
-				return new CompareToBuilder()
-				.append(empl.getPosition(), empl2.getPosition())
-				.append(empl.getName(), empl2.getName())
-				.append(empl.getId(), empl2.getId())
-				.append(empl.getSalary(), empl2.getSalary())
-				.toComparison();
-				}
-				};
-				public static Comparator<EmployeeList> CompareById = new Comparator<EmployeeList>() {
-					@Override
-					public int compare(EmployeeList empl, EmployeeList empl2) {
-					return new CompareToBuilder()
-					.append(empl.getId(), empl2.getId())
-					.append(empl.getName(), empl2.getName())
-					.append(empl.getPosition(), empl2.getPosition())
-					.append(empl.getSalary(), empl2.getSalary())
-					.toComparison();
-					}
-					};
-			
+import java.util.List;
 
-}
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class EmployeeList extends Employee {
+	static Employee[] emplyeesArray;
+	public static void main(String[] args) throws IOException, ParseException {
+		
+JSONParser parser = new JSONParser();	
+ObjectMapper mapper = new ObjectMapper();
+	String json = ((JSONArray) parser.parse(new FileReader(System.getProperty("user.dir") + "\\src\\" + "Employee1.json"))).toJSONString();
+	 
+	List<Employee> employees = Arrays.asList(mapper.readValue(json, Employee[].class));
+		
+	for (Employee Employee : employees) {
+		System.out.println("Employee name: " + Employee.getName() + ". ID: " +  Employee.getId() + ". position -> "
+	+ Employee.getPosition() + ". salary: " + Employee.getSalary());
+		}
+	
+	Employee[] emplyeesArray = (Employee[]) employees.toArray();
+	Arrays.sort(emplyeesArray);
+	
+	Arrays.sort(emplyeesArray, Employee.CompareByName);
+	for (Employee employee : emplyeesArray) {
+	System.out.println("Sorting by name : " + employee.getName() + " -> ID: " + employee.getId() + 
+			" " + employee.getPosition() + " : " + employee.getSalary());
+	}
+	
+	Arrays.sort(emplyeesArray, Employee.CompareBySalary);
+	for (Employee employee : emplyeesArray) {
+	System.out.println("Sorting by salary : " + employee.getName() + " -> ID: " + employee.getId() + 
+			" " + employee.getPosition() + " : " + employee.getSalary());
+	}
+	
+//	Arrays.sort(emplyeesArray, EmployeeList.CompareByPosition);
+//	for (EmployeeList employee : emplyeesArray) {
+//	System.out.println("Sorting by position : " + employee.getName() + " -> ID: " + employee.getId() + 
+//			" " + employee.getPosition() + " : " + employee.getSalary());
+//	}
+//	Arrays.sort(emplyeesArray, EmployeeList.CompareById);
+//	for (EmployeeList employee : emplyeesArray) {
+//	System.out.println("Sorting by ID : " + employee.getName() + " -> ID: " + employee.getId() + 
+//			" " + employee.getPosition() + " : " + employee.getSalary());
+//	}
+
+
+	}
+	}
+	
+ 
+
