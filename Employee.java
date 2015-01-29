2bc0fcae2c143ecf3419a624f36e019f4dac4b28
Aeternia-ua <gherasymuk@gmail.com>
2015-01-29 02:17:56 +0200

    test
    test

--- Employee.java
+++ Employee.java
@@ -1,50 +1,55 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
-public class Employee {
-	private EmployeeId id;
-    private EmployeeName name;
-    private EmployeeSalary salary;
-    private EmployeePosition position;
+public class Employee extends EmployeeList {
 
-    public Employee() {
-    }
-
-    public EmployeeId getId() {
-        return id;
-    }
-
-    public void setId(EmployeeId id) {
-        this.id = id;
-    }
-
-    public EmployeeName getName() {
-        return name;
-    }
-
-    public void setName(EmployeeName name) {
-        this.name = name;
-    }
-
-    public EmployeeSalary getSalary() {
-        return salary;
-    }
-
-    public void setSalary(EmployeeSalary salary) {
-        this.salary = salary;
-    }
-
-	public EmployeePosition getPosition() {
-		return position;
-	}
-
-	public void setPosition(EmployeePosition position) {
-		this.position = position;
+	public static void main(String[] args) throws IOException, ParseException {
+		
+JSONParser parser = new JSONParser();	
+ObjectMapper mapper = new ObjectMapper();
+	String json = ((JSONArray) parser.parse(new FileReader(System.getProperty("user.dir") + "\\src\\" + "Employee1.json"))).toJSONString();
+	 
+	List<EmployeeList> employees = Arrays.asList(mapper.readValue(json, EmployeeList[].class));
+		
+	for (EmployeeList Employee : employees) {
+		System.out.println("Employee name: " + Employee.getName() + ". ID: " +  Employee.getId() + ". position -> "
+	+ Employee.getPosition() + ". salary: " + Employee.getSalary());
+		}
+	
+	EmployeeList[] emplyeesArray = (EmployeeList[]) employees.toArray();
+	Arrays.sort(emplyeesArray);
+	
+	System.out.println ("Employees sorted by salary: ");
+	for (EmployeeList employee : emplyeesArray) {
+		System.out.println(employee.getId() + employee.getName() + " -> " + employee.getPosition() + " : " + employee.getSalary());
 	}
 	
+
+//	public List<Employee> sortByName (){
+//	Collections.sort(employees, new Comparator <Employee>() {
+//		@Override
+//		public int compare(Employee o1, Employee o2) {
+//			 return o1.getName().compareTo(o2.getName());
+//		}
+//	});
+//	return (List<Employee>) employees;
+//	}
 	}
-
-
-
-
+	}
+	
+ 
 
--- EmployeeList.java
+++ EmployeeList.java
@@ -1,20 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
+public class EmployeeList implements Comparable<EmployeeList> {
+	
+	private long id;
+	private String name;
+	private String position;
+	private long salary;
+	
+	public EmployeeList() {
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPosition() {
+		return position;
+	}
+	
+	public long getSalary() {
+		return salary;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	
+	
+	@Override
+	public int compareTo(EmployeeList o) {
+		return this.salary < o.getSalary() ? -1 :
+			this.salary > o.getSalary() ? 1 : 0;
+	}
+	
 
-import org.json.simple.JSONObject;
-
-
-public class EmployeeList {
-
-	    private List<Employee> employees = new ArrayList<Employee>();
-	    EmployeeList() {
-	    }
-	   
-		public List<Employee> getEmployees() {
-	        return employees;
-	    }
-	    public void setEmployees(List<Employee> employees) {
-	        this.employees = employees;
-	    }
-
+//	public EmployeeList sortById (){
+//		@Override
+//	public int compareTo(EmployeeList o) {
+//		return this.id < o.getId() ? -1 :
+//			this.id > o.getId() ? 1 : 0;
+//	  }
+//	}
+//	public List<Employee> sortByName (){
+	
 }
